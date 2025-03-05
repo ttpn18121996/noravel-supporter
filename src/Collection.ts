2b7b556a6fa05213efbcd713598953e35c44bdf8
ttpn18121996 <ttpn18121996@gmail.com>
2025-03-05 18:08:54 +0700

    Implements Iterator for Collection

--- src/Collection.ts
+++ src/Collection.ts
@@ -3,7 +3,7 @@ import Arr from './Arr';
 import Helper from './Support/Helper';
 import { Arrayable, Jsonable, Stringable } from './types';
 
-export default class Collection<T> implements Arrayable<T>, Jsonable, Stringable {
+export default class Collection<T> implements Arrayable<T>, Iterable<T>, Jsonable, Stringable {
   private items: T[] = [];
 
   public constructor(items: T[] = []) {
@@ -671,5 +671,9 @@ export default class Collection<T> implements Arrayable<T>, Jsonable, Stringable
     }
 
     return new Collection(item);
+  }
+
+  [Symbol.iterator](): Iterator<T> {
+    return this.items.values();
   }
 }
