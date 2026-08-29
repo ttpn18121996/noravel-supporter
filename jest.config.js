59456e71bcf97df5a5fdb86a09f4d7f4b4095426
ttpn18121996 <ttpn18121996@gmail.com>
2026-08-29 22:28:25 +0700

    fix: run-tests

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  testEnvironment: 'node',
+  moduleFileExtensions: ['js', 'ts', 'json', 'node'],
+  testMatch: ['**/test/**/*.test.js'],
+
+  transformIgnorePatterns: ['/node_modules/', '/dist/']
+};
